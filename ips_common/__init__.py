db9588b197ec63cab52145a019362e50d35e7501
Paul Soule <paul.soule@ext.ons.gov.uk>
2019-03-20 11:00:43 +0000

    Use PBR for PyPi build

--- ips_common/__init__.py
+++ ips_common/__init__.py
@@ -1,1 +1,6 @@
-name = "ips_common"
+from pbr.version import VersionInfo
+
+
+_v = VersionInfo('IPS_Common_Library').semantic_version()
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
-import setuptools
-
 #
+# Set the version tag: git tag -a 0.1.9 -m "version 0.1.9"
 # To build: python3 setup.py sdist bdist_wheel
 # Push to PyPy: twine upload dist/*
 #
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="ips_common",
-    version="0.1.8",
-    author="The Office for National Statistics, U.K.",
-    author_email="author@example.com",
-    description="Common functions for the ONS IPS Project",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-    install_requires=['PyYAML==5.1'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_data={
-        '': ['*.yaml']
-    }
-)
+import setuptools
+
+setuptools.setup(setup_requires=['pbr'], pbr=True)
